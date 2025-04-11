de315d65e230654650c93abb3cc62ac30adc1255
nabilghazir <nabil.ghaziro27@gmail.com>
2025-04-11 15:36:04 +0700

    deploy

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,17 @@ app.get('/', (req, res) => {
   res.send('Welcome to the API!');
 });
 
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
+
 app.use("/api",api);
 
 app.use((req, res) => {
   res.status(404).send('Not found BRUH');
 });
+
+setInterval(() => console.log('Server running...'), 10000);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
